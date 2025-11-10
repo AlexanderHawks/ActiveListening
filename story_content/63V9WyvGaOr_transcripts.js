bf9f683c2d4c8165ac0aa3c7f4bdeede84cda8b0
AlexanderHawks <157898165+AlexanderHawks@users.noreply.github.com>
2025-11-09 19:07:25 -0600

    Delete story_content directory

--- story_content/63V9WyvGaOr_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":0,"text":"I've been making my payments on time for\r\nyears,"},{"start":2880,"text":"but now I'm getting late fees that I don't\r\neven understand."},{"start":5900,"text":"I've always been loyal to you guys, and I\r\njust"},{"start":8220,"text":"expected a little more help with this."}]}]};
-    window.globalLoadJsAsset('story_content/63V9WyvGaOr_transcripts.js', JSON.stringify(data));
-})();
