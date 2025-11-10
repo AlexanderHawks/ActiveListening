a27318f255cbb2e60bc7ba79a8fc0a65262dd54c
AlexanderHawks <157898165+AlexanderHawks@users.noreply.github.com>
2025-11-09 19:11:33 -0600

    Delete Charges directory

--- Charges/story_content/6i9l5OEaDBs_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":0,"text":"I've been making my payments on time for\r\nyears,"},{"start":3600,"text":"but now I'm getting late fees that I don't\r\neven understand."},{"start":7000,"text":"I've always been loyal to you guys, and I\r\njust"},{"start":9420,"text":"expected a little more help with this."}]}]};
-    window.globalLoadJsAsset('story_content/6i9l5OEaDBs_transcripts.js', JSON.stringify(data));
-})();
